9c56c8d51f9d4e578158dc983d796870cfed4b8a
amirfarahani68 <drfarahani.amir@gmail.com>
2026-04-06 13:42:53 -0700

    feat(api): add AuthController

---
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Post, Get, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+
+@Controller('api/v1/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMe(@Request() req) {
+    return this.authService.me(req.user);
+  }
+}
