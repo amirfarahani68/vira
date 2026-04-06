3b4dd8ac5f8dd64ac1a296748ce8b736164b9e8c
amirfarahani68 <drfarahani.amir@gmail.com>
2026-04-06 11:05:14 -0700

    Add web index page

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>ویرا</h1>
+      <p>این صفحهٔ اصلی برنامه حضور و غیاب ویرا است.</p>
+    </div>
+  );
+}
